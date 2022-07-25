f82263d7a070a73fade30c57cfba28466a1ef7d0
Logan Price <logan.price@blackboard.com>
2022-07-25 11:33:27 -0400

    feat: exercise 5

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -59,8 +59,8 @@ const MainHeader = styled.div`
   display: flex;
   align-items: baseline;
   padding: 18px 32px;
-  height: 72px;
   border-bottom: 1px solid ${COLORS.gray[300]};
+  overflow-x: auto;
 
   @media ${QUERIES.tabletAndDown} {
     border-top: 4px solid ${COLORS.gray[900]};
@@ -76,7 +76,7 @@ const MainHeader = styled.div`
 
 const DesktopNav = styled.nav`
   display: flex;
-  gap: 48px;
+  gap: clamp(1rem, 8.1vw - 4rem, 3rem);
   margin: 0px 48px;
 
   @media ${QUERIES.tabletAndDown} {
@@ -106,6 +106,7 @@ const RightSide = styled.div`
 `;
 
 const LeftSide = styled.div`
+  flex: 1;
   @media ${QUERIES.tabletAndDown} {
     display: none;
   }
